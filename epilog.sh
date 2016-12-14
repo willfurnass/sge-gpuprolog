be74019cf90d7bcee99b76f52c652e493a0207e7
Mozhgan Chimeh <mozhgan.kabiri@gmail.com>
2016-12-14 12:09:44 +0000

    comments made

--- epilog.sh
+++ epilog.sh
@@ -1,32 +1,22 @@
 #!/bin/sh
 #
-# Finish script to release GPU devices.
+# Authors: Mozhgan Kabiri Chimeh, Paul Richmond
+# Contact: m.kabiri-chimeh@sheffield.ac.uk
 #
+# Epilog script to free GPU lock files for devices used by a job.
 # Based on https://github.com/kyamagu/sge-gpuprolog
+#
 
-:'
-# Check if the environment file is readable.
-ENV_FILE=$SGE_JOB_SPOOL_DIR/environment
-if [ ! -f $ENV_FILE -o ! -r $ENV_FILE ]
-then
-  exit 1
-fi
-'
 
-:'
-device_ids=$(grep SGE_GPU $ENV_FILE | \
-             sed -e "s/,/ /g" | \
-             sed -n "s/SGE_GPU=\(.*\)/\1/p" | \
-             xargs shuf -e)
-'
-
-# Remove lock files.
+# Reformat the list of device ids used by the job (into space seperated)
 device_ids=$(echo $CUDA_DEVICE_VISIBLE | sed -e "s/,/ /g")
 
+# Loop through through the device IDs and free the lockfile
 for device_id in $device_ids
 do
-  #lockfile=/tmp/lock-nvidia$device_id
+  #lock file is specific for each ShARC node and each device combination (node_number is a SGE prolog variable)
   lockfile=/tmp/lock_$(node_number)_device$(device_id)
+  # Check dir exists then remove the lockfile
   if [ -d $lockfile ]
   then
     rmdir -f $lockfile
--- prolog.sh
+++ prolog.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 #
-# Startup script to allocate GPU devices.
+# Authors: Mozhgan Kabiri Chimeh, Paul Richmond
+# Contact: m.kabiri-chimeh@sheffield.ac.uk
 #
+# Startup script to allocate GPU devices.
 # Based on https://github.com/kyamagu/sge-gpuprolog
+#
 
-# Query how many gpus to allocate.
+# Query how many gpus to allocate.Using qstat
 NGPUS=$(qstat -j $JOB_ID | \
         sed -n "s/hard resource_list:.*gpu=\([[:digit:]]\+\).*/\1/p")
 if [ -z $NGPUS ] # check if NGPUS is null, then exit
@@ -15,28 +18,26 @@ if [ $NGPUS -le 0 ]  # check if NGPUS are less than equal 0, then exit
 then
   exit 0
 fi
-NGPUS=$(expr $NGPUS \* ${NSLOTS=1})
 
-:'
-# Check if the environment file is writable.
-ENV_FILE=$SGE_JOB_SPOOL_DIR/environment
-if [ ! -f $ENV_FILE -o ! -w $ENV_FILE ]
-then
-  exit 1
-fi
-'
 
-# Allocate and lock GPUs.
+# Allocate and lock GPUs. We will populate SGE_GPU with the device IDs that the job should use.
 SGE_GPU=""
+# Counter for free devices that we have obtained a lock on
 i=0
+# Get a list of all device IDS which will be space seperated
 device_ids=$(nvidia-smi -L | cut -f1 -d":" | cut -f2 -d" " | xargs shuf -e)
+
+#loop through the device IDs and check to see if a lock can be obtained for the device
 for device_id in $device_ids
 do
-  #lockfile=/tmp/lock-gpu$device_id
+  #lock file is specific for each ShARC node and each device combination (node_number is a SGE prolog variable)
   lockfile=/tmp/lock_$(node_number)_device$(device_id)
+  #use mkdir to obtain a lock (will fail if file exists)
   if mkdir $lockfile
   then
+    # We have obtained a lock so can have exclusive access to this GPU id. Add the ID to SGE_GPU
     SGE_GPU="$SGE_GPU $device_id"
+    # Increment i counter to reflect that we have obtained a GPU for the job
     i=$(expr $i + 1)
     if [ $i -ge $NGPUS ] # check if reserved num gpus are greater than requested number of GPUS
     then
@@ -45,19 +46,18 @@ do
   fi
 done
 
-if [ $i -lt $NGPUS ] # check if reserved num gpus are less than requested number of GPUS
+ # Check if reserved num gpus are less than requested number of GPUS. If this is true then there were not enough free devices for the job and the scheduling should fail
+if [ $i -lt $NGPUS ]
 then
   echo "ERROR: Only reserved $i of $NGPUS requested devices."
   exit 1
 fi
 
-:'
-echo SGE_GPU="$(echo $SGE_GPU | sed -e 's/^ //' | sed -e 's/ /,/g')" >> $ENV_FILE
-'
 
-# no need to write into the enviroment file as above, we simply set the set cuda devices visible
+# Set the cuda devices visible. This will re-enumerate the devices to users. i.e. a job requesting 1 device which locks device_id=3 will see this as device 0 in nvidia-smi
 SGE_GPU="$(echo $SGE_GPU | sed -e 's/^ //' | sed -e 's/ /,/g')" # seperating device_ids with comma
 
 # Set the environment.
-export CUDA_DEVICE_VISIBLE=$SGE_GPU # you need to source this (use source to execute it), so that the variable modified by the script will be available after the script completes
+# IMPORTANT: You need to source this script (use source to execute it), so that the variable modified by the script will be available after the script completes
+export CUDA_DEVICE_VISIBLE=$SGE_GPU 
 exit 0
