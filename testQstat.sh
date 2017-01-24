4c07eda413d1b643d5ec88be5af1a26d89ec0690
Will Furnass <will@thearete.co.uk>
2017-01-24 11:50:45 +0000

    Minor formatting fixes

--- testQstat.sh
+++ testQstat.sh
@@ -5,7 +5,7 @@
 export PATH=$PATH:. 
 
 NGPU=$(qstat -j $1 | \
-        sed -n "s/hard resource_list:.*gpu=\([[:digit:]]\+\).*/\1/p")
+       sed -n "s/hard resource_list:.*gpu=\([[:digit:]]\+\).*/\1/p")
 
 echo $NGPU
 
