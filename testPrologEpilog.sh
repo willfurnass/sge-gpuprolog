f1c562ab19b4932bb8030eaec1147e52e7472739
Will Furnass <will@thearete.co.uk>
2017-01-24 12:58:03 +0000

    Minor changes to test script

--- testPrologEpilog.sh
+++ testPrologEpilog.sh
@@ -1,35 +1,53 @@
 #!/bin/bash
 #
-# Authors: Mozhgan Kabiri Chimeh, Paul Richmond
+# Authors: Mozhgan Kabiri Chimeh, Paul Richmond, Will Furnass
 # Contact: m.kabiri-chimeh@sheffield.ac.uk
 #
 # Startup script to allocate GPU devices.
 # Based on https://github.com/kyamagu/sge-gpuprolog
 #
-# The script is written to test if both Prolog and Epilog scripts are working
-# For this purpose, we created another script called 'qstat' to emulate what the real 'qstat' command does.
-# To execute, simply run ./testPriloEpilog $1 , where $1 is the number of gpus. This is actually the $JOB_ID, but we as we use a fake one, we set this as the number of GPUs user requested
+# The script is written to test if both the prolog and epilog scripts are working.
+# For this purpose, we created another script called 'qstat' to 
+# emulate what the real 'qstat' command does.
+#
+# Requires the 'deviceQuery' program included with the CUDA samples
 
+export SGE_GPU_LOCKS_DIR=/tmp/sge-gpu-test
+export SGE_JOB_SPOOL_DIR=/tmp/dummy-sge-spool-dir 
 # The system will look for executables in current directory without a "./". 
-export PATH=$PATH:. 
+export PATH=${PATH}:. 
 
+usage() {
+    echo "Usage: $0 <num-gpus> <cuda-deviceQuery-prog>" 1>&2
+    exit 1
+}
 
+# Check the command-line arguments
+[[ $# -eq 2 ]] || usage
+[[ $1 -gt 0 ]] || usage
+# Reuse $JOB_ID: set this as the number of GPUs user requested (WF: ?)
 JOB_ID=$1 # num gpus
+DEVICE_QUERY="$2"
+[[ -x $DEVICE_QUERY ]] || usage
+
+# DELETEME?
+# To execute, simply run ./testPrologEpilog.sh $1, where $1 is the number of gpus. 
+# DELETEME?
 
 # Output the directory status list for lock files
 check_locks_func() {
-  echo
-  echo "------------------------------------"
-  for device_id in $device_ids; do
-    lockfile="/tmp/sge-gpu/lock_device_$device_id"
-    if [[ -d $lockfile ]]; then 
-      echo "$lockfile exists"
-    else
-      echo "no lock exists on $lockfile"
-    fi
-  done
-  echo "------------------------------------"
-  echo
+    echo
+    echo "------------------------------------"
+    for device_id in $device_ids; do
+        lockfile="${SGE_GPU_LOCKS_DIR}/lock_device_$device_id"
+        if [[ -d $lockfile ]]; then 
+            echo "Lock file $lockfile exists"
+        else
+            echo "Lock file $lockfile does not exist"
+        fi
+    done
+    echo "------------------------------------"
+    echo
 }
 
 # Output current list of devices IDS from nvidia-smi
@@ -41,32 +59,28 @@ deviceId_func() {
 #################################################
 
 # Create directory to store lock files
-mkdir -p /tmp/sge-gpu
-find /tmp/sge-gpu/ -type d -exec rmdir {} \;
-mkdir -p /tmp/dummy-sge-spool-dir
-find /tmp/dummy-sge-spool-dir/ -type f -exec rm {} \;
+mkdir -p $SGE_GPU_LOCKS_DIR
+find $SGE_GPU_LOCKS_DIR -mindepth 1 -type d -empty -delete
 
-# Create dummy Sun Grid Engine job spool directory
-export $SGE_JOB_SPOOL_DIR=/tmp/dummy-sge-spool-dir 
+# Create dummy Sun Grid Engine job spool directory and job environment file
 mkdir -p $SGE_JOB_SPOOL_DIR
 truncate --size=0 $SGE_JOB_SPOOL_DIR/environment
 
-echo "START ...."
+echo "START ..."
 export JOB_ID=$1 # num gpus
 
 deviceId_func
 check_locks_func
 
-echo "executing Prolog ..................."
-echo "source ./prolog.sh"
+echo "Executing prolog script as a subprocess: "
 bash prolog.sh
 
-# setting NVIDIA sample path to check the number of visible devices, make sure to compile/build the deviceQuery before hand
-~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery/deviceQuery  -noprompt | egrep "^Device"
+# Display the number of visible devices
+$DEVICE_QUERY -noprompt | egrep "^Device"
 check_locks_func
 
-echo "executing Epilog ..................."
-bash ./epilog.sh
+echo "Executing epilog script"
+bash epilog.sh
 
 check_locks_func
 echo "DONE"
